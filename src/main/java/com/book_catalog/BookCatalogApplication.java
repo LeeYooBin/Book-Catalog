0eb5cc0bedfa64ea2c2cd5f10bb2389bec5bc021
LeeYooBin <santosroberdan25@gmail.com>
2024-07-12 23:55:47 -0300

    Feat: Implementing main application class with CommandLineRunner integration

--- src/main/java/com/book_catalog/BookCatalogApplication.java
+++ src/main/java/com/book_catalog/BookCatalogApplication.java
@@ -1,13 +1,29 @@
 package com.book_catalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.book_catalog.view.App;
+
 @SpringBootApplication
-public class BookCatalogApplication {
+public class BookCatalogApplication implements CommandLineRunner {
+  @Autowired
+  private App app;
+
+  public BookCatalogApplication (App app) {
+    this.app = app;
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookCatalogApplication.class, args);
 	}
+
+  @Override
+  public void run(String... args) throws Exception {
+
+    app.init();
+  }
 
 }
